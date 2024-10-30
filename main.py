0750dc654c5c67553876511700e2e0890ecee560
RAKTIM PAL <nzymepal1991@gmail.com>
2024-10-30 17:10:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import zipfile
 import re
 import shutil
 from PIL import Image
+import requests
+from io import BytesIO
 
-# Page setup
 # Page setup
 st.set_page_config(page_title="Oracle BIP IntelliScan Tool", page_icon="🔍")
 
-# Display two images side by side with controlled width
+# Display two images side by side with fixed size
 col1, col2 = st.columns(2)
 with col1:
-    st.image("https://img.freepik.com/free-vector/cute-bot-say-users-hello-chatbot-greets-online-consultation_80328-195.jpg", 
-             caption="Chatbot Greeting", use_column_width="always")
+    img1_url = "https://img.freepik.com/free-vector/cute-bot-say-users-hello-chatbot-greets-online-consultation_80328-195.jpg"
+    img1 = Image.open(BytesIO(requests.get(img1_url).content))
+    st.image(img1, width=250)
+
 with col2:
-    st.image("https://blog.logomaster.ai/hs-fs/hubfs/ibm-logo-2.jpg?width=672&height=448&name=ibm-logo-2.jpg", 
-             caption="IBM Logo", use_column_width="always")
+    img2_url = "https://blog.logomaster.ai/hs-fs/hubfs/ibm-logo-2.jpg?width=672&height=448&name=ibm-logo-2.jpg"
+    img2 = Image.open(BytesIO(requests.get(img2_url).content))
+    st.image(img2, width=250)
 
 # Custom CSS styling
 st.markdown("""
